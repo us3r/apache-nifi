ed72442a75e4ffe5eb0050ddddea19fe10cd8ae8
Mariusz Derela <mariusz.derela@gmail.com>
2017-03-08 16:01:09 +0100

    small changes in syntax

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -15,15 +15,15 @@ function set_nifi_properties()  {
 function set_java() {
    JAVA_MS=${JAVA_MS:=1024}
    JAVA_MX=${JAVA_MX:=1024}
-   sed -e "s/Xms.*/Xms${JAVA_MS}m/" -e "s/Xms.*/Xms${JAVA_MX}m/" -i $NIFI_HOME/conf/bootstrap.conf
+   sed -e "s/Xms.*/Xms${JAVA_MS}m/" -e "s/Xmx.*/Xmx${JAVA_MX}m/" -i $NIFI_HOME/conf/bootstrap.conf
 }
 
 function set_hdfs() {
  if [ $HDFS ]; then
    HDFS_URI=${HDFS_URI:=http://hdfs.marathon.mesos:9000/v1/connection}
-   mkdir $NIFI_HOME/hdfs
-   curl $HDFS_URI/core-site.xml > $NIFI_HOME/hdfs/core-site.xml
-   curl $HDFS_URI/hdfs-site.xml > $NIFI_HOME/hdfs/hdfs-site.xml
+   mkdir -p $NIFI_HOME/hdfs
+   wget -O $NIFI_HOME/hdfs/core-site.xml $HDFS_URI/core-site.xml
+   wget -O $NIFI_HOME/hdfs/hdfs-site.xml $HDFS_URI/hdfs-site.xml
  fi
 }
 

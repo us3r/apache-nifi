4c8aab213667c8823cb5d6a1bf1067818a87db4f
Mariusz Derela <mariusz.derela@gmail.com>
2017-03-08 10:04:26 +0100

    add hdfs support

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,6 +12,23 @@ function set_nifi_properties()  {
     IFS=$SAVEIFS
 }
 
+function set_java() {
+   JAVA_MS=${JAVA_MS:=1024}
+   JAVA_MX=${JAVA_MX:=1024}
+   sed -e "s/Xms.*/Xms${JAVA_MS}m/" -e "s/Xms.*/Xms${JAVA_MX}m/" -i $NIFI_HOME/conf/bootsrap.conf
+}
+
+function set_hdfs() {
+ if [ $HDFS ]; then
+   HDFS_URI=${HDFS_URI:=http://hdfs.marathon.mesos:9000/v1/connection}
+   mkdir $NIFI_HOME/hdfs
+   curl $HDFS_URI/core-site.xml > $NIFI_HOME/hdfs/core-site.xml
+   curl $HDFS_URI/hdfs-site.xml > $NIFI_HOME/hdfs/hdfs-site.xml
+ fi
+}
+
 set_nifi_properties
+set_java
+set_hdfs
 
 exec "$@"
